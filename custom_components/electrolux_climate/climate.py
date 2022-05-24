98e972a0ac5109128c0b7161b1a1a28a8b76ab55
Jose Manuel <joskfg@gmnail.com>
2022-05-24 20:45:01 +0200

    Dummy climate implementation

--- custom_components/electrolux_climate/climate.py
+++ custom_components/electrolux_climate/climate.py
@@ -6,6 +6,7 @@ from esphome.const import CONF_AWAY_CONFIG, CONF_COOL_ACTION, \
     CONF_DEFAULT_TARGET_TEMPERATURE_HIGH, CONF_DEFAULT_TARGET_TEMPERATURE_LOW, CONF_HEAT_ACTION, \
     CONF_ID, CONF_IDLE_ACTION, CONF_SENSOR
 
+
 electrolux_climate_ns = cg.esphome_ns.namespace('electrolux_climate')
 
 ElectroluxClimate = electrolux_climate_ns.class_('ElectroluxClimate', climate.Climate, cg.Component)
@@ -15,7 +16,7 @@ CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend({
 }).extend(cv.COMPONENT_SCHEMA)
 
 
-# def to_code(config):
-#     var = cg.new_Pvariable(config[CONF_ID])
-#     yield cg.register_component(var, config)
-#     yield binary_sensor.register_binary_sensor(var, config)
+def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    yield cg.register_component(var, config)
+    yield climate.register_climate(var, config)
--- custom_components/electrolux_climate/electrolux_climate.h
+++ custom_components/electrolux_climate/electrolux_climate.h
@@ -4,38 +4,39 @@
 namespace esphome {
 namespace electrolux_climate {
 
-class ElectroluxClimate : public Component, public climate::Climate {
- public:
-  void setup() override {
-    // This will be called by App.setup()
-  }
-  void control(const ClimateCall &call) override {
-    if (call.get_mode().has_value()) {
-      // User requested mode change
-      ClimateMode mode = *call.get_mode();
-      // Send mode to hardware
-      // ...
-      ESP_LOGD("custom", "Mode: %s", mode.c_str());
+class ElectroluxClimate : public climate::Climate, public Component {
+  public:
+    void setup() override {
+      // This will be called by App.setup()
+    }
+  protected: 
+    void control(const climate::ClimateCall &call) override {
+      if (call.get_mode().has_value()) {
+        // User requested mode change
+        climate::ClimateMode mode = *call.get_mode();
+        // Send mode to hardware
+        // ...
+        ESP_LOGD("custom", "Mode: %s", climate_mode_to_string(mode));
 
-      // Publish updated state
-      this->mode = mode;
-      this->publish_state();
+        // Publish updated state
+        this->mode = mode;
+        this->publish_state();
+      }
+      if (call.get_target_temperature().has_value()) {
+        // User requested target temperature change
+        float temp = *call.get_target_temperature();
+        // Send target temp to climate
+        // ...
+        ESP_LOGD("custom", "Target temperature: %.1f", temp);
+      }
     }
-    if (call.get_target_temperature().has_value()) {
-      // User requested target temperature change
-      float temp = *call.get_target_temperature();
-      // Send target temp to climate
-      // ...
-      ESP_LOGD("custom", "Target temperature: %.1f", temp);
+    climate::ClimateTraits traits() override {
+      // The capabilities of the climate device
+      auto traits = climate::ClimateTraits();
+      traits.set_supports_current_temperature(false);
+      traits.set_supported_modes({climate::CLIMATE_MODE_HEAT_COOL});
+      return traits;
     }
-  }
-  ClimateTraits traits() override {
-    // The capabilities of the climate device
-    auto traits = climate::ClimateTraits();
-    traits.set_supports_current_temperature(true);
-    traits.set_supported_modes({climate::CLIMATE_MODE_HEAT_COOL});
-    return traits;
-  }
 };
 
 } //namespace electrolux_climate
