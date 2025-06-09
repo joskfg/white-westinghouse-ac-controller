ee394d2aaa7b8220742c6e389c0d59cc8e7b62a6
Jose Manuel <joskfg@gmnail.com>
2025-06-09 17:22:06 +0200

    Init test

--- components/electrolux_climate/climate.py
+++ components/electrolux_climate/climate.py
@@ -5,14 +5,19 @@ from esphome.const import CONF_ID
 
 AUTO_LOAD = ["climate_ir"]
 
-electrolux_climate_ns = cg.esphome_ns.namespace('electrolux_climate')
+electrolux_climate_ns = cg.esphome_ns.namespace("electrolux_climate")
 
-ElectroluxClimate = electrolux_climate_ns.class_('ElectroluxClimate', climate_ir.ClimateIR, cg.Component)
+ElectroluxClimate = electrolux_climate_ns.class_(
+    "ElectroluxClimate", climate_ir.ClimateIR
+)
 
-CONFIG_SCHEMA = climate_ir.CLIMATE_IR_SCHEMA.extend({
-    cv.GenerateID(): cv.declare_id(ElectroluxClimate),
-}).extend(cv.COMPONENT_SCHEMA)
+CONFIG_SCHEMA = climate_ir.CLIMATE_IR_SCHEMA.extend(
+    {
+        cv.GenerateID(): cv.declare_id(ElectroluxClimate),
+    }
+)
 
-def to_code(config):
+
+async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    yield climate_ir.register_climate_ir(var, config)
+    await climate_ir.register_climate_ir(var, config)
