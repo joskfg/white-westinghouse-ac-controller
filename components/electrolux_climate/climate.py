0ab1749f443b04f64bf251d76d66ef55af2408a2
joskfg <joskfg@gmail.com>
2025-06-10 20:52:28 +0200

    Upgrade component to latest ESPHome (2025.5.2) (#1)
    * Upgrade component to be compatible with the latest ESPHome interface (2025.5.2)
    
    ---------
    
    Co-authored-by: Jose Manuel <joskfg@gmnail.com>

--- components/electrolux_climate/climate.py
+++ components/electrolux_climate/climate.py
@@ -1,18 +1,15 @@
 import esphome.codegen as cg
-import esphome.config_validation as cv
 from esphome.components import climate_ir
-from esphome.const import CONF_ID
 
 AUTO_LOAD = ["climate_ir"]
 
-electrolux_climate_ns = cg.esphome_ns.namespace('electrolux_climate')
+electrolux_climate_ns = cg.esphome_ns.namespace("electrolux_climate")
 
-ElectroluxClimate = electrolux_climate_ns.class_('ElectroluxClimate', climate_ir.ClimateIR, cg.Component)
+ElectroluxClimate = electrolux_climate_ns.class_(
+    "ElectroluxClimate", climate_ir.ClimateIR
+)
 
-CONFIG_SCHEMA = climate_ir.CLIMATE_IR_SCHEMA.extend({
-    cv.GenerateID(): cv.declare_id(ElectroluxClimate),
-}).extend(cv.COMPONENT_SCHEMA)
+CONFIG_SCHEMA = climate_ir.climate_ir_schema(ElectroluxClimate)
 
-def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    yield climate_ir.register_climate_ir(var, config)
+async def to_code(config):
+    await climate_ir.new_climate_ir(config)
